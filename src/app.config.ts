21e1e15de84cd246f1255b6e65588aa4876eb386
vriskaserket51 <serketvriska51@gmail.com>
2024-03-03 19:02:44 +0900

    change auth to env; rm express

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,14 +1,12 @@
 import config from "@colyseus/tools";
 import { monitor } from "@colyseus/monitor";
-import express from "express";
 import basicAuth from "express-basic-auth";
-import path from "path";
 
 import { GameRoom } from "./rooms/GameRoom";
 
 const basicAuthMiddleware = basicAuth({
   users: {
-    admin: "admin",
+    [`${process.env.ID}`]: `${process.env.PW}`,
   },
 
   challenge: true,
@@ -20,13 +18,6 @@ export default config({
   },
 
   initializeExpress: (app) => {
-    app.use(
-      "/",
-      express.static(
-        path.join(__dirname, "..", "..", "resistance-avalon-online-web", "dist")
-      )
-    );
-
     app.use("/admin", basicAuthMiddleware, monitor());
   },
 
